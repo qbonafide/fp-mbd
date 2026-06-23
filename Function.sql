279986e39f5a956aae904a5cc96975cf36a07c28
sayyidil24 <sayyidilanam5@gmail.com>
2026-06-24 02:08:16 +0700

    Function.sql

--- Function.sql
+++ Function.sql
@@ -7,72 +7,60 @@ DELIMITER $$
 CREATE FUNCTION fn_hitung_skor_risiko_pelanggan(
     p_id_pelanggan INT
 )
-    
 RETURNS INT
 DETERMINISTIC
 BEGIN
 
     DECLARE v_skor INT DEFAULT 0;
+    DECLARE v_jumlah INT DEFAULT 0;
 
+    -- 1. Riwayat keterlambatan pengembalian
+    SELECT COUNT(*)
+    INTO v_jumlah
+    FROM Kontrak_Sewa
+    WHERE id_pelanggan = p_id_pelanggan
+      AND status_sewa = 'Terlambat';
 
-    SELECT 
-        COALESCE(SUM(la.skor_risiko),0)
-    INTO v_skor
-    FROM log_anomali la
-    JOIN kontrak_sewa ks
-        ON la.id_sewa = ks.id_sewa
+    SET v_skor = v_skor + (v_jumlah * 20);
+
+    -- 2. Riwayat pembayaran menunggak
+    SELECT COUNT(*)
+    INTO v_jumlah
+    FROM Pembayaran pb
+    JOIN Kontrak_Sewa ks
+        ON pb.id_sewa = ks.id_sewa
+    WHERE ks.id_pelanggan = p_id_pelanggan
+      AND pb.status_pembayaran = 'Menunggak';
+
+    SET v_skor = v_skor + (v_jumlah * 15);
+
+    -- 3. Pelanggaran geofence
+    SELECT COUNT(*)
+    INTO v_jumlah
+    FROM Pelanggaran_Geofence pg
+    JOIN Kontrak_Sewa ks
+        ON pg.id_sewa = ks.id_sewa
     WHERE ks.id_pelanggan = p_id_pelanggan;
 
+    SET v_skor = v_skor + (v_jumlah * 10);
 
-    SELECT 
-        v_skor + (COUNT(*) * 50)
-    INTO v_skor
-
-    FROM kontrak_sewa ks
-
-    JOIN kendaraan k
-        ON ks.id_kendaraan = k.id_kendaraan
-
-    WHERE ks.id_pelanggan = p_id_pelanggan
-
-    AND k.status_kendaraan = 'Hilang';
-
-
+    -- 4. Status akun pelanggan
     IF EXISTS (
-
         SELECT 1
-
-        FROM pelanggan p
-
-        JOIN daftar_hitam dh
-
-        ON p.nik = dh.nik
-
-        WHERE p.id_pelanggan = p_id_pelanggan
-
-        AND dh.status_verifikasi='Terverifikasi'
-
-    )
-
-    THEN
-
-        SET v_skor = v_skor + 50;
-
+        FROM Pelanggan
+        WHERE id_pelanggan = p_id_pelanggan
+          AND status_akun IN ('Ditangguhkan', 'Diblokir')
+    ) THEN
+        SET v_skor = v_skor + 25;
     END IF;
 
-
+    -- Batas maksimum skor
     IF v_skor > 100 THEN
-
         SET v_skor = 100;
-
     END IF;
-
-
 
     RETURN v_skor;
 
-
 END$$
-
 
 DELIMITER ;
